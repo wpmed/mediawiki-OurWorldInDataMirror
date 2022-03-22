474a5906bb7564e050da69dbe4d25fdedf3c36b7
tim-moody <tim@timmoody.com>
2022-03-22 12:42:32 -0400

    add support for full urls, trim input

--- src/Hooks.php
+++ src/Hooks.php
@@ -29,22 +29,27 @@ class Hooks implements ParserFirstCallInitHook {
          * @return array HTML that will not be processed further
          */
         public function renderOurWorldInDataMirror( string $input, array $args, Parser $parser, PPFrame $frame ) {
+                // check for non-url parts (also removes space after graph url)
+                $inp_parts = preg_split("/\\s/", $input);
+                $graph_url = $inp_parts[0];
+                $graph_path = $graph_url;
                 // check if we were given a full URL (possibly with parameters)
-                $bits = wfParseUrl( $input );
-                if (
-                        $bits !== false
-                        && $bits['host'] === 'owidm.wmcloud.org'
-                        && preg_match( ',^/grapher/(.*)$,', $bits['path'], $matches )
-                ) {
-                        $input = $matches[1];
-
-                        if ( isset( $bits['query'] ) ) {
+                // assume any host is owidm
+                // path may or may not begin with /grapher/ but everything following taken as graph path
+                // even the presence of ?tab=map will cause the input to parse as a url
+                $url_parts = wfParseUrl( $graph_url );
+                if ( $url_parts !== false ) {
+                        if ( preg_match( ',^/grapher/(.*)$,', $url_parts['path'], $matches ))
+                                $graph_path = $matches[1];
+                        else
+                                $graph_path = $url_parts['path'];
+                        if ( isset( $url_parts['query'] ) ) {
                                 // args passed explicitly into the tag override args present in the URL's query string
-                                $args = array_merge( wfCgiToArray( $bits['query'] ), $args );
+                                $args = array_merge( wfCgiToArray( $url_parts['query'] ), $args );
                         }
                 }
 
-                $baseUrl = 'https://owidm.wmcloud.org/grapher/' . rawurlencode( $input );
+                $baseUrl = 'https://owidm.wmcloud.org/grapher/' . rawurlencode( $graph_path );
                 $url = wfAppendQuery( $baseUrl, $args );
                 $parser->getOutput()->addModuleStyles( [ 'ext.owid' ] );
                 return [
